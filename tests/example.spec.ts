f15a148269c76a6806ddb06dbc1cf889a663464d
manimaranb07 <manimaranmca0710@gmail.com>
2026-09-02 20:23:48 +0530

    Updated comments example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
-
+//test runner 1
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
-
+//test runner 2
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
